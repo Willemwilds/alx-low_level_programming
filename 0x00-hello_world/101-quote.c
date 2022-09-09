d842297bd6b7afd14f3c3bcc0e0705d079a090ae
Williams <willemwilds@gmail.com>
2022-09-08 18:36:50 -0700

    Standard output

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
+* main - Prints to the Standard error
 *
-* Return: Always 0 (Success)
+* Return: one
 */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - \+
-Dora Korpar, 2015-10-19\n");
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
