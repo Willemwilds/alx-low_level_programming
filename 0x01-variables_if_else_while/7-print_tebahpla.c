a850c5cea1c3a4f386c47a8d7f4972e81faef1b8
Williams <willemwilds@gmail.com>
2022-09-12 08:16:17 -0700

    Alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ *main - Prints the alphabets in reverse order
+ *
+ *Return - always 0 (Success)
+ */
+
+int main(void)
+{
+
+char letter;
+for (letter = 'z'; letter >= 'a', letter--;)
+{
+putchar(letter);
+}
+
+putchar('\n');
+return (0);
+
+}
