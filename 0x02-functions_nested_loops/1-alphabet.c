72e640749ce897263061620113761db528cd9ea7
Williams <willemwilds@gmail.com>
2022-09-13 12:05:10 -0700

    Prints numbers with coma

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,14 +4,14 @@
 *main - check the code
 *
 *Return: Always 0
- */
+*/
 
 
 void print_alphabet(void)
 {
 
 char ch;
-for (ch = 'a'; ch = 'z'; ch++)
+for (ch = 'a'; ch <= 'z'; ch++)
 {
 putchar(ch);
 }
