7526c441310bddb0e39771b5d366b91c307a420c
Williams <willemwilds@gmail.com>
2022-09-13 12:45:05 -0700

    Alphabet function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+*main - Print alphabet
+*
+* *Return: return void
+*/
+
+
+void print_alphabet(void)
+{
+
+
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+
+}
+
+
+int main(void)
+{
+
+print_alphabet();
+
+return (0);
+
+}
