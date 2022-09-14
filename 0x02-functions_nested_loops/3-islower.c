67a5c871220cc414031d0eaeb2518cb5407a996b
Williams <willemwilds@gmail.com>
2022-09-14 15:17:03 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*_islower()- print numbers in lower cases
+*
+*Return: return 1 if lowercase otherwise 0.
+*
+*@c: input parameter
+*/
+
+
+
+int _islower(int c)
+{
+
+	if (c == 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+
+}
