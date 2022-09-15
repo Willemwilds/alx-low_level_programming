611218622fd7b35683d0f3651d52405772ce5049
Williams <willemwilds@gmail.com>
2022-09-15 08:18:16 -0700

    print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*print_most_numbers- Check for digits
+*
+*
+*Return: Always 0.
+*/
+
+
+
+void print_most_numbers(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		if (num != '2' && num != 4)
+			_putchar(num);
+	}
+
+	_putchar ('\n');
+
+}
