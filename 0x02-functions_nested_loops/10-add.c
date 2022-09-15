816d403b5c21ab29ef9adad2c2b2cfad9c395467
Williams <willemwilds@gmail.com>
2022-09-14 18:14:58 -0700

    Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*add()- Adds two digits
+*
+*Return: returns sum
+*
+*@n1: input
+*
+*@n2: input
+*/
+
+
+
+int add(int n1, int n2)
+{
+	int sum = n1 + n2;
+
+	return (sum);
+
+}
