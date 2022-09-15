ed9c5e246a2b7dccf2a6afb893efbd51b932dd8e
Williams <willemwilds@gmail.com>
2022-09-15 14:10:47 -0700

    print_line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+*print_line- Draws line
+*
+*Return: Always 0.
+*/
+
+
+
+void print_line(int n)
+{
+
+	char c = '-';
+
+	for (c <= 0; c <= 'n'; c++)
+	{
+		if (c <= 0)
+			_putchar('\n');
+		else
+			_putchar(c);
+
+		_putchar(c);
+
+	}
+
+
+
+}
