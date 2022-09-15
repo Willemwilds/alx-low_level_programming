878c7bb22156bbfb56ddd8d99045e03f9aea3361
Williams <willemwilds@gmail.com>
2022-09-15 07:41:29 -0700

    print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*print_numbers- Check for digits
+*
+*
+*Return: Always 0.
+*/
+
+
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		_putchar(num);
+
+	_putchar('\n');
+
+}
