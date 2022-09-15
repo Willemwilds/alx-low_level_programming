580a0d2354075acbdfc684fcc25d610b2bd5f95f
Williams <willemwilds@gmail.com>
2022-09-15 12:30:41 -0700

    more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+*more_numbers- Check for digits
+*
+*Return: Always 0.
+*/
+
+
+
+void more_numbers(void)
+{
+
+	int x;
+	int y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{	
+			_putchar(y);
+		
+		}
+
+		_putchar(x);
+	}
+
+	_putchar('\n');
+
+
+
+}
