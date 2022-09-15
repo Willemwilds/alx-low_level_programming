dd6b4f80186fa3cf7b09f3dc4151d0c004429b79
Williams <willemwilds@gmail.com>
2022-09-15 07:29:49 -0700

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*mul- Check for digits
+*
+*@a: input
+*
+*@b: input
+*
+*Return: returns int
+*/
+
+
+
+int mul(int a, int b)
+{
+
+	int m;
+
+	m = a * b;
+
+	return (m);
+
+}
