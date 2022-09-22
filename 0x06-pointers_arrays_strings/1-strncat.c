a6c2c9a8bade21875c1f29ec225f1d86bd61bbc2
Williams <willemwilds@gmail.com>
2022-09-22 03:33:41 -0700

    More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+*_strcat- concatenates two strings
+*@dest: input
+*@src: input
+*@int: input
+*
+*Return: void
+*/
+
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+	int x;
+
+	int y;
+
+	i = 0;
+	while (dest[x] != '\0')
+	{
+		x++;
+
+	}
+
+	y = 0;
+	while (y < n && src[y] != '\0')
+	{
+
+		dest[i] = src[j];
+
+		x++;
+
+		y++;
+	}
+	dest[x] = '\0';
+	return (dest);
+
+
+
+
+
+
+}
