6e6e34db379395eb724c80ce4de43d0f3c5b03ff
Williams <willemwilds@gmail.com>
2022-09-26 16:10:12 -0700

    Pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *_strpbrk- compares string
+ *@s: pointer string
+ *@accept: pointer
+ *
+ *Return: return memory address
+ */
+
+
+
+
+char *_strpbrk(char *s, char *accept)
+{
+
+	int i;
+
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		if (accept[i] == *s)
+
+			return (s);
+
+	}
+	return (0);
+
+}
