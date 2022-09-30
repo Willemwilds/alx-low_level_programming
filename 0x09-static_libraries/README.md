5cdbd0d0b58c65e267a147dd046401edb4f380b0
Williams <willemwilds@gmail.com>
2022-09-30 16:29:10 -0700

    Static library

---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+This directory contains a static library and a shell script that creates a static library
---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	printf("I am creating my first static library\n");
+
+	return (0);
+}
