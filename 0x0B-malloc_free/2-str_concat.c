5f7e9a38f1cd5486668029e2da8263ace90d7532
Williams <willemwilds@gmail.com>
2022-10-03 11:29:48 -0700

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -34,21 +34,19 @@ char *str_concat(char *s1, char *s2)
 	if (new_str == NULL)
 		return (NULL);
 
-	int a = 0, b = 0;
+	i = j = 0;
 
-	while (s1[a] != '\0')
+	while (s1[i] != '\0')
 	{
-		new_str[b] = s1[a];
-		a++;
-		b++;
+		new_str[i] = s1[i];
+		i++;
 	}
-	while (s2[a] != '\0')
+	while (s2[j] != '\0')
 	{
-		new_str[b] = s2[a];
-		a++;
-		b++;
+		new_str[i] = s2[j];
+		i++, j++;
 	}
-	new_str[b] = '\0';
+	new_str[i] = '\0';
 
 	return (new_str);
 }
