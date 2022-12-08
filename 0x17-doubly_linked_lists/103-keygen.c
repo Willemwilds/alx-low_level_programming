3ef2301680b62bd6025c6b2447117de8e3f432a8
Williams <willemwilds@gmail.com>
2022-12-08 07:06:49 -0800

    Doubly linked list

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - keygen print a valid key for the username
+ *
+ * Return: key of the username
+ **/
+
+int main(void)
+{
+	return (key_t)
+}
